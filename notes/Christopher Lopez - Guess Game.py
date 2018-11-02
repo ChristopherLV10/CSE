2c4f1070191f4b2ceef1fa56d8aa171e1c45bd4f
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-11-02 09:45:43 -0700

    guess game continued

--- notes/Christopher Lopez - Guess Game.py
+++ notes/Christopher Lopez - Guess Game.py
@@ -1,7 +1,9 @@
 print("Guess Game!")
 print()
-print()
 
-name = input("Guess a number 1-10")
+name = input("Guess a number 1-10:")
+
 import random
-print(random.randint(0,10))
+print(random.randint(0, 10))
+
+while  and  :
