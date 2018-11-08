68ca73a08018dd4577a13eebd924cc4e38e46461
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-11-08 09:45:14 -0800

    started lucky 7s

---
+++ notes/Christopher Lopez - Lucky 7s.py
@@ -0,0 +1,16 @@
+import random
+Dice 1 = random.randint(1,6)
+Dice 2 = random.randint(1,6)
+
+print("Lucky 7s!")
+print()
+
+money = 15
+
+ if
+     Dice 1 += Dice 2 = 7
+     print("You win 4 dollars")
+     money = money + 4
+ else:
+     print("You lose 1 dollar")
+     money = money - 1
