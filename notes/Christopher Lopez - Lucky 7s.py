652efce1fd76405efc74b7b243b988b5925f9e07
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-11-13 09:44:52 -0800

    lucky 7s almost done

--- notes/Christopher Lopez - Lucky 7s.py
+++ notes/Christopher Lopez - Lucky 7s.py
@@ -7,16 +7,21 @@ Dice_2 = random.randint(1, 6)
 print("Lucky 7s!")
 print("Round: 1")
 
+playing = True
 round = 1
 money = 15
 
 if Dice_1 + Dice_2 == 7:
      print("You win 4 dollars")
      round = round + 1
-     print("Round:",round)
+     print("Round:", round)
      money = money + 4
 else:
      print("You lose 1 dollar")
      round = round + 1
      print("Round:",round)
      money = money - 1
+
+if  money == 0:
+     print("You ran out of money on Round", round)
+     playing = False
