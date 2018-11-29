68067e86c35cb2f15af684c8b88464cf5d23a4b2
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-11-29 09:45:53 -0800

    mad libs

--- notes/Christopher Lopez - Mad Lib.py
+++ notes/Christopher Lopez - Mad Lib.py
@@ -1,6 +1,22 @@
 adjective = input("Type in an adjective:")
 noun = input("Type in a noun:")
+adjective2 = input("Type in an adjective:")
+verb = input("Type in a verb:")
 
-print("I saw a %s %s" % (adjective, noun))
-
+print("A vacation is when you take a trip to some %s place" % adjective)
+print("with your %s family. Usually you go to some place" % adjective2)
+print("that is near a/an __________________ .")
+A good vacation place is one where you can ride __________________
+or play __________________ or go hunting for __________________ . I like
+to spend my time ________________________ or ________________________.
+When parents go on a vacation, they spend their time eating
+three __________________ a day, and fathers play golf, and mothers
+sit around ________________________. Last summer, my little brother
+fell in a/an __________________ and got poison __________________ all
+over his________________________. My family is going to go to (the)
+__________________, and I will practice ________________________. Parents
+need vacations more than kids because parents are always very
+__________________ and because they have to work__________________
+hours every day all year making enough __________________ to pay
+for the vacation.")
 
