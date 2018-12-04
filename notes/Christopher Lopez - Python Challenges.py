fcbd6009bee7d4dd5f436efb4c3987ddb52a840b
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-12-04 09:45:26 -0800

    Challenges

--- notes/Christopher Lopez - Python Challenges.py
+++ notes/Christopher Lopez - Python Challenges.py
@@ -1,5 +1,11 @@
 def challenge1(firstname, lastname):
-    return 
+    print(challenge1)
 
 
-print(challenge1)
+Vowel = {"a", "e", "i", "o", "u"}
+alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"}
+print("".join(alphabet))
+if
+
+
