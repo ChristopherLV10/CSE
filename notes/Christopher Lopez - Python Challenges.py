56c3a6b21bb85620dc586aa1ad5d74f0e72008df
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2018-12-06 09:45:50 -0800

    challenges

--- notes/Christopher Lopez - Python Challenges.py
+++ notes/Christopher Lopez - Python Challenges.py
@@ -1,17 +1,19 @@
+import random
+
+
 def challenge1(firstname, lastname):
     print(challenge1)
 
 
 Vowel = {"a", "e", "i", "o", "u"}
 
+
 print(" Is a vowel, ".join(Vowel))
 
 
-import random
 integer = random.randint(-10, 10)
 print(integer)
 if integer >= 0:
     print("positive")
 else:
     print("negative")
-
