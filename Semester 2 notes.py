c752a33f5abbc85157f2133e706a96e54147ace0
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2019-01-08 09:45:10 -0800

    semester 2 notes

---
+++ Semester 2 notes.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+# Hello
+
+cars = 5
+driving = True
+print("I have %d cars" % cars)
+
+age = input("How old are you")
+print(age + "?! You really shouldnt lie about your age")
+
+colors = ["blue", "red", "white", "black", "brown"]
+colors.append("cyan")
+print(colors)
+colors.pop(0)
+print(colors)
+print(len(colors))
