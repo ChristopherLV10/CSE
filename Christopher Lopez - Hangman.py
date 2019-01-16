a4031d4ef333b1ff2d5591053d6b0bf58788cf09
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2019-01-16 09:45:19 -0800

    hangman

--- Christopher Lopez - Hangman.py
+++ Christopher Lopez - Hangman.py
@@ -1,15 +1,26 @@
 import random
 hangman_words = ["Grapefruit", "Pineapple", "Blueberries", "Apples", "Mango", "Strawberries", "Lemons", "Watermelon",
                  "Oranges"]
-word = []
+letters_guessed = []
 guesses = 8
 playing = True
-random.choice(hangman_words)
-input("Enter a letter:")
+answer = random.choice(hangman_words)
+solved = False
+
 while guesses > 0 and playing:
-    if input == random.choice:
-        word. append(input)
-    else:
+    print(letters_guessed)
+    for i in range(len(hangman_words)):
+        if hangman_words[i] == "letter":
+            hangman_words.pop(i)
+            hangman_words.insert(i, "*")
+            print(answer)
+
+    letter = input("Enter a letter:")
+    letters_guessed.append(letter)
+    if letter not in answer:
         guesses = guesses - 1
-        input("Enter a letter:")
-print(guesses)
+        print("Wrong")
+        print("Guesses Left:", guesses)
+    else:
+        print(answer)
+
