0a43f27cb0d6ffc400110f290644bdab83106dc9
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2019-02-05 09:44:35 -0800

    World Map

--- Christopher Lopez - Adventure Game.py
+++ Christopher Lopez - Adventure Game.py
@@ -1,1 +1,21 @@
-print("Welcome to ")
+print("Welcome to The Vault of Glass!")
+
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
+world_map = {
+    'VOG Entrance': {
+        'NAME': "Entrance",
+        'DESCRIPTION': "In front of the entrance to the vault.",
+        'PATHS': {
+            'WEST': "Left Button"
+
+        }
+    },
+    'Left Button': {
+        'NAME': "Left Button",
+        'DESCRIPTION': "Here is one of the 2 buttons you need to press to open the door",
+        'PATHS': {
+            'EAST': 'VOG Entrance'
+        }
+    }
+}
