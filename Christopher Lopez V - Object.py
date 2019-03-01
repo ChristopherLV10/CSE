23df7fa39f31e2fbe9ae211df64c2cc3d561e4b5
Christopher Lopez Verduzco 4601 <fkno@fresnou.org>
2019-03-01 10:39:14 -0800

    OBjeCt

--- Christopher Lopez V - Object.py
+++ Christopher Lopez V - Object.py
@@ -1,6 +1,9 @@
-class Gun(object):
+class Weapon(object):
     def __init__(self, shoot, reload, bullets_left=30):
         self.shoot = shoot
         self.reload = reload
         self.bullets_left = bullets_left
 
+    def press_trigger(self):
+        bullets_left -= 1
+        print("")
