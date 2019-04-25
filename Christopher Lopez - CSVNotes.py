e73f7d89d21512feadb328eae4acbf71162ac0f8
ChristopherLV10 <44374237+ChristopherLV10@users.noreply.github.com>
2019-04-25 09:47:46 -0700

    notes

--- Christopher Lopez - CSVNotes.py
+++ Christopher Lopez - CSVNotes.py
@@ -1,7 +1,27 @@
 import csv
 
-with open("Book1.csv") as old_csv:
-    reader = csv.reader(old_csv)
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing file...  ")
+        writer = csv.writer(new_csv)
+        reader = csv.reader(old_csv)
     for row in reader:
-        old_number = row[0]
-        print(old_number)
+        old_number = int(row[0])
+        new_number = old_number + 1
+        row[0] = new_number
+        writer. writerow(row)
+
+print("OK")
+
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing file...  ")
+        writer = csv.writer(new_csv)
+        reader = csv.reader(old_csv)
+    for row in reader:
+        old_number = int(row[0])
+        first_num = int(old_number[0])
+        if first_num % 2 == 3:
+        writer. writerow(row)
+
+print("OK")
