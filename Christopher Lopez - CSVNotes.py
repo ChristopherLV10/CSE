aaaab54143b45878180d3b202f364c387e4c2d88
ChristopherLV10 <44374237+ChristopherLV10@users.noreply.github.com>
2019-04-26 09:48:18 -0700

    Notes

--- Christopher Lopez - CSVNotes.py
+++ Christopher Lopez - CSVNotes.py
@@ -1,27 +1,41 @@
 import csv
 
+
+def validate(num: str):
+    if not all_16_degits(num):
+        return False
+    if divisible_by_2(num) and divisible_by_3(num):
+        return True
+    return False
+
+
+def divisible_by_3(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def divisible_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+def all_16_digits(num: str):
+
+
+
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
         print("Writing file...  ")
         writer = csv.writer(new_csv)
         reader = csv.reader(old_csv)
-    for row in reader:
-        old_number = int(row[0])
-        new_number = old_number + 1
-        row[0] = new_number
-        writer. writerow(row)
+        print("Processing...")
 
-print("OK")
-
-with open("Book1.csv", 'r') as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file...  ")
-        writer = csv.writer(new_csv)
-        reader = csv.reader(old_csv)
     for row in reader:
-        old_number = int(row[0])
+        old_number = row[0]
         first_num = int(old_number[0])
-        if first_num % 2 == 3:
-        writer. writerow(row)
-
-print("OK")
+        if validate(old_number):
+            writer.writerow(row)
+print("Done")
