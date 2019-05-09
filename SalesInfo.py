67f267b79db797f8ffc4c369ce53e300f3fac5b8
ChristopherLV10 <44374237+ChristopherLV10@users.noreply.github.com>
2019-05-09 09:46:35 -0700

    saleS

--- SalesInfo.py
+++ SalesInfo.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("Sales Records", "r") as old_csv:
+with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
     for row in reader:
         thing = row[13]
