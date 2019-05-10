5d25aca45394e17a92750275b7a77fb86e6d315d
ChristopherLV10 <44374237+ChristopherLV10@users.noreply.github.com>
2019-05-10 10:39:19 -0700

    SAleS

--- SalesInfo.py
+++ SalesInfo.py
@@ -2,6 +2,13 @@ import csv
 
 with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
+    fruit_total = 0
     for row in reader:
         thing = row[13]
-        print(thing)
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Fruits':
+            fruit_total += float(thing)
+print(fruit_total)
+
+
